0c4f5239cd22c61d85f82d5236300f72367225aa
krzysztof-stanislawski <krzysztofstanislawski@getklarity.io>
2024-01-08 00:22:03 +0100

    upload 3rdParty lib

---
+++ src/Application/RegisteredObject.h
@@ -0,0 +1,26 @@
+//
+// Created by pbialas on 29.11.22.
+//
+#pragma  once
+
+#include <iostream>
+#include <list>
+
+class RegisteredObject {
+
+
+public:
+    static void add(RegisteredObject *p);
+
+    static void cleanup();
+
+    RegisteredObject() {
+        RegisteredObject::add(this);
+    }
+
+    virtual ~RegisteredObject() = default;
+
+
+private:
+    static std::list<RegisteredObject *> registry_;
+};
